3419f6c13f3d9659c24feadd11413ccc66b77542
Xenophmix <xenophmix@gmail.com>
2023-02-16 09:49:21 +0800

    完成最新消息及購物流程功能

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
                                 <a href="?do=login">會員登入</a> |
                                 <a href="?do=admin">管理登入</a>
                         </div>
-                        情人節特惠活動 &nbsp; 為了慶祝七夕情人節，將舉辦情人兩人到現場有七七折之特惠活動~
+                        <marquee> 年終特賣會開跑了&nbsp;&nbsp;&nbsp;&nbsp;情人節特惠活動</marquee>
                 </div>
                 <div id="left" class="ct">
                         <div style="min-height:400px;">
@@ -39,11 +39,11 @@
                 </div>
                 <div id="right">
                         <?php
-                        $do = $_GET['do']??'main';
+                        $do = $_GET['do'] ?? 'main';
                         $file = "./front/" . $do . ".php";
-                        if(file_exists($file)){
+                        if (file_exists($file)) {
                                 include $file;
-                        }else{
+                        } else {
                                 include "./front/main.php";
                         }
                         ?>
