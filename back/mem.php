1880ffcd84701c2ece7860f29caf12ee4f740b15
Xenophmix <xenophmix@gmail.com>
2023-02-17 08:32:51 +0800

    複製管理者管理頁面並修改成會員管理頁面

---
+++ back/mem.php
@@ -0,0 +1,28 @@
+<div class="ct">
+  <button onclick="location.href='?do=new_admin'">會員管理</button>
+</div>
+
+<table class="all ct">
+  <tr class="tt">
+    <td>姓名</td>
+    <td>會員帳號</td>
+    <td>註冊日期</td>
+    <td>操作</td>
+  </tr>
+  <?php
+  $rows = $Mem->all();
+  foreach ($rows as $row) {
+  ?>
+    <tr class="pp">
+      <td><?= $row['name'] ?></td>
+      <td><?= $row['acc'] ?></td>
+      <td><?= $row['reg_date'] ?></td>
+      <td>
+        <button onclick="location.href='?do=edit_admin&id=<?= $row['id'] ?>'">修改</button>
+        <button onclick="del('admin',<?= $row['id'] ?>)">刪除</button>
+      </td>
+    </tr>
+  <?php
+  }
+  ?>
+</table>
