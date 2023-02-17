d2cd6671f49757132d6bd54c0a33a950eddb3c46
Xenophmix <xenophmix@gmail.com>
2023-02-17 09:28:41 +0800

    建立商品分類畫面

---
+++ back/th.php
@@ -0,0 +1,30 @@
+<h2 class="ct">商品分類</h2>
+<div class="ct">
+  <input type="text" name="big" id="big">
+  <button>新增</button>
+</div>
+<div class="ct">
+  新增中分類
+  <select name="sec" id="sec">
+
+  </select>
+  <input type="text" name="mid" id="mid">
+  <button>新增</button>
+</div>
+<table>
+  <tr class="tt">
+    <td>asd</td>
+    <td class="ct">
+      <button>修改</button>
+      <button>刪除</button>
+    </td>
+  </tr>
+
+  <tr class="pp ct">
+    <td>asd</td>
+    <td>
+      <button>修改</button>
+      <button>刪除</button>
+    </td>
+  </tr>
+</table>
