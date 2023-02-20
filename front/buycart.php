96de0017291211e095588323d285401e5b5b7c58
Xenophmix <xenophmix@gmail.com>
2023-02-20 15:30:41 +0800

    增加判斷使用者沒有點商品就到購物車的情形

--- front/buycart.php
+++ front/buycart.php
@@ -1,7 +1,4 @@
 <?php
-if (empty($_SESSION['cart'])) {
-  echo "<h2 class='ct'>未選購任何商品</h2>";
-}
 
 if (isset($_GET['id'])) {
   $_SESSION['cart'][$_GET['id']] = $_GET['qt'];
@@ -21,32 +18,44 @@ if (!isset($_SESSION['mem'])) {
 ?>
 
 <h2 class="ct"><?= $_SESSION['mem'] ?>的購物車</h2>
+<?php
+if (!isset($_SESSION['cart'])) {
+  echo "<h2 class='ct'>未選購任何商品</h2>";
+} else {
 
-<table class="all">
-  <tr class="tt ct">
-    <td>編號</td>
-    <td>商品名稱</td>
-    <td>數量</td>
-    <td>庫存量</td>
-    <td>單價</td>
-    <td>小計</td>
-    <td>刪除</td>
-  </tr>
-  <?php
-  foreach ($_SESSION['cart'] as $id => $qt)
-    $goods = $Goods->find($id);
-  ?>
-  <tr class="pp ct">
-    <td><?= $goods['no']; ?></td>
-    <td><?= $goods['name']; ?></td>
-    <td><?= $qt; ?></td>
-    <td><?= $goods['stock']; ?></td>
-    <td><?= $goods['price']; ?></td>
-    <!--將商品單價乘上購買的數量成為小計-->
-    <td><?= $goods['price'] * $qt; ?></td>
-    <td>
-      <!--建立刪除商品的點擊函式-->
-      <img src="icon/0415.jpg" onclick="delCart(<?= $goods['id']; ?>)">
-    </td>
-  </tr>
-</table>
+?>
+  <table class="all">
+    <tr class="tt ct">
+      <td>編號</td>
+      <td>商品名稱</td>
+      <td>數量</td>
+      <td>庫存量</td>
+      <td>單價</td>
+      <td>小計</td>
+      <td>刪除</td>
+    </tr>
+    <?php
+    foreach ($_SESSION['cart'] as $id => $qt)
+      $goods = $Goods->find($id);
+    ?>
+    <tr class="pp ct">
+      <td><?= $goods['no']; ?></td>
+      <td><?= $goods['name']; ?></td>
+      <td><?= $qt; ?></td>
+      <td><?= $goods['stock']; ?></td>
+      <td><?= $goods['price']; ?></td>
+      <!--將商品單價乘上購買的數量成為小計-->
+      <td><?= $goods['price'] * $qt; ?></td>
+      <td>
+        <!--建立刪除商品的點擊函式-->
+        <img src="icon/0415.jpg" onclick="delCart(<?= $goods['id']; ?>)">
+      </td>
+    </tr>
+  </table>
+  <div class="ct">
+    <img src="./icon/0411.jpg">
+    <img src="./icon/0412.jpg">
+  </div>
+<?php
+}
+?>
